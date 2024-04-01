55d75187636bdc9b12d19d20c1771dac13611618
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-01 00:26:44 +0000

    0x00. Pagination Mandatory Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,4 +11,4 @@ def index_range(page: int, page_size: int) -> tuple:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return tuple(start_index, end_index)
+    return tuple([start_index, end_index])
