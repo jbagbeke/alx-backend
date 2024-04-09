2ef270e51e4adc65551a82a75caf35d51d8a8e22
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-09 23:08:58 +0000

    0x02. i18n Mandatory Task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Basic Flask setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Babel Config Class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Babel Locale Selector
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def root_func() -> str:
+    """
+    Basic flask app root
+    """
+    return render_template('0-index.html')
