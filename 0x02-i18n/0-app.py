78f1571e63905894d5faa866f7631478ac08a207
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-09 22:18:10 +0000

    0x02. i18n Mandatory Task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Basic Flask setup
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root_func():
+    """
+    Basic flask app root
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
